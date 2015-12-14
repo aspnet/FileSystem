b15b31bb1902736fcc70c6b6f5fe549f1cb500cc
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2015-12-14 09:24:03 +0100

    Fix formatting

--- src/Microsoft.AspNet.FileProviders.Sources/Implementation/EmptyDisposable.cs
+++ src/Microsoft.AspNet.FileProviders.Sources/Implementation/EmptyDisposable.cs
@@ -8,6 +8,7 @@ namespace Microsoft.AspNet.FileProviders
     internal class EmptyDisposable : IDisposable
     {
         public static EmptyDisposable Instance { get; } = new EmptyDisposable();
+
         private EmptyDisposable()
         {
 
