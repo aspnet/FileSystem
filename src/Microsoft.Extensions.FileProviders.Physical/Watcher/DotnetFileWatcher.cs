c9901b4cd881a842aba9d32dabba3751e1ad5f18
Victor Hurdugaci <victor.hurdugaci@microsoft.com>
2016-04-08 15:50:18 -0700

    Polling file watcher

---
+++ src/Microsoft.Extensions.FileProviders.Physical/Watcher/DotnetFileWatcher.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+
+namespace Microsoft.Extensions.FileProviders.Physical.Watcher
+{
+    internal class DotnetFileWatcher : IFileSystemWatcher
+    {
+        private readonly string _watchedDirectory;
+        private FileSystemWatcher _fileSystemWatcher;
+
+        public DotnetFileWatcher(string watchedDirectory)
+        {
+            if (string.IsNullOrEmpty(watchedDirectory))
+            {
+                throw new ArgumentNullException(nameof(watchedDirectory));
+            }
+
+            _watchedDirectory = watchedDirectory;
+            CreateFileSystemWatcher();
+        }
+
+        private void FSW_Error(object sender, ErrorEventArgs e)
+        {
+            // Recreate the watcher
+            CreateFileSystemWatcher();
+        }
+
+        private void FSW_Renamed(object sender, RenamedEventArgs e)
+        {
+            NotifyChange(e.OldFullPath);
+            NotifyChange(e.FullPath);
+        }
+
+        private void FSW_Changed(object sender, FileSystemEventArgs e)
+        {
+            NotifyChange(e.FullPath);
+        }
+
+        private void NotifyChange(string fullPath)
+        {
+            // Only report file changes
+            if (File.Exists(fullPath))
+            {
+                OnFileChange(fullPath);
+            }
+        }
+
+        private void CreateFileSystemWatcher()
+        {
+            bool enableEvents = false;
+
+            if (_fileSystemWatcher != null)
+            {
+                enableEvents = _fileSystemWatcher.EnableRaisingEvents;
+
+                _fileSystemWatcher.EnableRaisingEvents = false;
+
+                _fileSystemWatcher.Created -= FSW_Changed;
+                _fileSystemWatcher.Deleted -= FSW_Changed;
+                _fileSystemWatcher.Changed -= FSW_Changed;
+                _fileSystemWatcher.Renamed -= FSW_Renamed;
+                _fileSystemWatcher.Error -= FSW_Error;
+
+                _fileSystemWatcher.Dispose();
+            }
+
+            _fileSystemWatcher = new FileSystemWatcher(_watchedDirectory);
+            _fileSystemWatcher.IncludeSubdirectories = true;
+
+            _fileSystemWatcher.Created += FSW_Changed;
+            _fileSystemWatcher.Deleted += FSW_Changed;
+            _fileSystemWatcher.Changed += FSW_Changed;
+            _fileSystemWatcher.Renamed += FSW_Renamed;
+            _fileSystemWatcher.Error += FSW_Error;
+
+            _fileSystemWatcher.EnableRaisingEvents = enableEvents;
+        }
+
+        public bool EnableRisingEvents
+        {
+            get { return _fileSystemWatcher.EnableRaisingEvents; }
+            set { _fileSystemWatcher.EnableRaisingEvents = value; }
+        }
+
+        public event Action<string> OnFileChange;
+
+        public void Dispose()
+        {
+            _fileSystemWatcher.Dispose();
+        }
+    }
+}
---
+++ src/Microsoft.Extensions.FileProviders.Physical/Watcher/PollingFileWatcher.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace Microsoft.Extensions.FileProviders.Physical.Watcher
+{
+    internal class PollingFileWatcher : IFileSystemWatcher
+    {
+        // The minimum interval to rerun the scan
+        private static readonly TimeSpan _minRunInternal = TimeSpan.FromSeconds(.5);
+
+        private readonly string _watchedDirectory;
+
+        private Dictionary<string, FileMeta> _knownFiles = new Dictionary<string, FileMeta>();
+        private Dictionary<string, FileMeta> _tempDictionary = new Dictionary<string, FileMeta>();
+
+        private Thread _pollingThread;
+        private bool _raiseEvents;
+
+        private bool _disposed;
+
+        public PollingFileWatcher(string watchedDirectory)
+        {
+            if (string.IsNullOrEmpty(watchedDirectory))
+            {
+                throw new ArgumentNullException(nameof(watchedDirectory));
+            }
+
+            _watchedDirectory = watchedDirectory;
+
+            _pollingThread = new Thread(new ThreadStart(PollingLoop));
+            _pollingThread.IsBackground = true;
+            _pollingThread.Name = nameof(PollingFileWatcher);
+            _pollingThread.Start();
+        }
+
+        public event Action<string> OnFileChange;
+
+        public bool EnableRisingEvents
+        {
+            get
+            {
+                return _raiseEvents;
+            }
+            set
+            {
+                EnsureNotDisposed();
+
+                if (value == true)
+                {
+                    CreateKnownFilesSnapshot();
+
+                    if (_pollingThread.ThreadState == System.Threading.ThreadState.Unstarted)
+                    {
+                        // Start the loop the first time events are enabled
+                        _pollingThread.Start();
+                    }
+                }
+                _raiseEvents = value;
+            }
+        }
+
+        private void PollingLoop()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+
+            while (!_disposed)
+            {
+                if (stopwatch.Elapsed < _minRunInternal)
+                {
+                    // Don't run to often
+                    // The min wait time here can be double
+                    // the value of the variable (FYI)
+                    Thread.Sleep(_minRunInternal);
+                }
+
+                stopwatch.Reset();
+
+                if (!_raiseEvents)
+                {
+                    continue;
+                }
+
+                CheckForChangedFiles();
+            }
+
+            stopwatch.Stop();
+        }
+
+        private void CreateKnownFilesSnapshot()
+        {
+            _knownFiles.Clear();
+
+            ForeachFileInDirectory(f =>
+            {
+                _knownFiles.Add(f.FullName, new FileMeta(f.LastWriteTime));
+            });
+        }
+
+        private void CheckForChangedFiles()
+        {
+            ForeachFileInDirectory(f =>
+            {
+                var fullFilePath = f.FullName;
+
+                if (!_knownFiles.ContainsKey(fullFilePath))
+                {
+                    // New file
+                    NotifyChange(fullFilePath);
+                }
+                else
+                {
+                    var fileMeta = _knownFiles[fullFilePath];
+                    if (fileMeta.LastWriteTime != f.LastWriteTime)
+                    {
+                        // File changed
+                        NotifyChange(fullFilePath);
+                    }
+
+                    _knownFiles[fullFilePath] = new FileMeta(fileMeta.LastWriteTime, true);
+                }
+
+                _tempDictionary.Add(f.FullName, new FileMeta(f.LastWriteTime));
+            });
+
+            foreach (var file in _knownFiles)
+            {
+                if (!file.Value.FoundAgain)
+                {
+                    // File deleted
+                    NotifyChange(file.Key);
+                }
+            }
+
+            // Swap the two dictionaries
+            var swap = _knownFiles;
+            _knownFiles = _tempDictionary;
+            _tempDictionary = swap;
+
+            _tempDictionary.Clear();
+        }
+
+        private void ForeachFileInDirectory(Action<FileInfo> fileAction)
+        {
+            var watchedFiles = Directory.EnumerateFiles(_watchedDirectory, "*.*", SearchOption.AllDirectories);
+            foreach (var fullFilePath in watchedFiles)
+            {
+                fileAction(new FileInfo(fullFilePath));
+            }
+        }
+
+        private void NotifyChange(string fullPath)
+        {
+            if (!_disposed && _raiseEvents)
+            {
+                OnFileChange(fullPath);
+            }
+        }
+
+        private void EnsureNotDisposed()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(nameof(PollingFileWatcher));
+            }
+        }
+
+        public void Dispose()
+        {
+            EnableRisingEvents = false;
+            _disposed = true;
+        }
+
+        private struct FileMeta
+        {
+            public FileMeta(DateTime lastWriteTime, bool foundAgain = false)
+            {
+                LastWriteTime = lastWriteTime;
+                FoundAgain = foundAgain;
+            }
+
+            public DateTime LastWriteTime;
+
+            public bool FoundAgain;
+        }
+    }
+}
